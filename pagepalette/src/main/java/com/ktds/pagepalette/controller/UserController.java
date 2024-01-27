b85aff1330e673f957067776a044871b171e6efb
김지희 <cjg05034@naver.com>
2024-01-27 23:58:10 +0900

    feat: add check email API in UserController

--- pagepalette/src/main/java/com/ktds/pagepalette/controller/UserController.java
+++ pagepalette/src/main/java/com/ktds/pagepalette/controller/UserController.java
@@ -4,19 +4,17 @@ import com.ktds.pagepalette.dto.user.UserJoinReq;
 import com.ktds.pagepalette.dto.user.UserLoginReq;
 import com.ktds.pagepalette.dto.user.UserLoginRes;
 import com.ktds.pagepalette.service.UserService;
+import jakarta.websocket.server.PathParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(path="/user")
+@RequestMapping(path = "/user")
 public class UserController {
     private final UserService userService;
 
@@ -28,11 +26,14 @@ public class UserController {
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody UserLoginReq request) throws Exception {
         Optional<UserLoginRes> response = userService.login(request);
-        if(response.isEmpty()){
+        if (response.isEmpty()) {
             return new ResponseEntity<>("login fail", HttpStatus.FORBIDDEN);
         }
         return new ResponseEntity<>(response.get(), HttpStatus.ACCEPTED);
     }
 
-
+    @GetMapping("/check")
+    public ResponseEntity<?> checkEmail(@RequestParam("email") String email) {
+        return new ResponseEntity<>(userService.checkEmail(email), HttpStatus.ACCEPTED);
+    }
 }
