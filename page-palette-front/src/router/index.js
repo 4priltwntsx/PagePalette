7e04879e4d4c4ddff5ebec2a0f126af8e6b8c981
김지희 <cjg05034@naver.com>
2024-01-30 00:13:34 +0900

    feat: add Board component file

--- page-palette-front/src/router/index.js
+++ page-palette-front/src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Home from '../components/Home.vue'
 import Login from '../components/Login.vue'
 import NotFound from '../components/NotFound.vue'
+import Board from '../components/Board.vue'
 
 Vue.use(VueRouter)
 
@@ -19,9 +20,12 @@ const router = new VueRouter({
       component: Login
     },
     {
+      path:'/b/:bid', component:Board
+  },
+    {
       path: '*',
       component: NotFound
-    }
+      }
   ]
 })
 
