d8b1c4a0dd29d77d937ba016d48dcb7b5c0a56ce
jihee <4priltwntsx@naver.com>
2024-01-29 16:54:00 +0900

    feat: add BoardReadResponse Dto

--- pagepalette/src/main/java/com/ktds/pagepalette/dto/board/BoardReadRes.java
+++ pagepalette/src/main/java/com/ktds/pagepalette/dto/board/BoardReadRes.java
@@ -1,4 +1,20 @@
 package com.ktds.pagepalette.dto.board;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
 public class BoardReadRes {
+    Long boardId;
+    String title;
+    String bgColor;
+    String userEmail;
+
+    @Builder
+    public BoardReadRes(Long boardId, String title, String bgColor, String userEmail) {
+        this.boardId = boardId;
+        this.title = title;
+        this.bgColor = bgColor;
+        this.userEmail = userEmail;
+    }
 }
