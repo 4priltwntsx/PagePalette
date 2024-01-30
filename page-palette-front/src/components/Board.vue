6f7548e5e234879a54d9936201dc79f645bef256
김지희 <cjg05034@naver.com>
2024-01-30 23:51:25 +0900

    feat: add routing code in front prjt folder

--- page-palette-front/src/components/Board.vue
+++ page-palette-front/src/components/Board.vue
@@ -1,23 +1,41 @@
 <template>
-    <div>
-        Board
-        <div>bid: {{ bid}} </div>
+  <div>
+    Board
+    <div v-if="loading">loading board...</div>
+    <div v-else>
+      <div>bid: {{ bid }}</div>
+      <router-link :to="`/b/${bid}/c/1`">Card 1 </router-link>
+      <router-link :to="`/b/${bid}/c/2`">Card 2 </router-link>
     </div>
+    <hr />
+    <router-view> </router-view>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            bid:0
-        }
-    },
-    created() {
-        this.bid = this.$route.params.bid
+  data() {
+    return {
+      bid: 0,
+      loading: true
+    };
+  },
+
+  created() {
+    this.fetchData()
+  },
+
+  methods:{
+    fetchData(){
+        this.loading = true
+        setTimeout(()=>{ // 로딩 지연 시간 0.5초 정도, 초기 화면 로딩 때 loading board 출력 이후 fetchData, bid 세팅, loading - false
+            this.bid = this.$route.params.bid;
+            this.loading = false
+        }, 500)
+        
     }
-}
+  }
+};
 </script>
 
-
-<style>
-</style>
+<style></style>
--- page-palette-front/src/router/index.js
+++ page-palette-front/src/router/index.js
@@ -1,33 +1,38 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../components/Home.vue'
-import Login from '../components/Login.vue'
-import NotFound from '../components/NotFound.vue'
-import Board from '../components/Board.vue'
-
-Vue.use(VueRouter)
-
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../components/Home.vue";
+import Login from "../components/Login.vue";
+import NotFound from "../components/NotFound.vue";
+import Board from "../components/Board.vue";
+import Card from "../components/Card.vue";
+Vue.use(VueRouter);
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   routes: [
     {
-      path: '/',
-      component:Home
+      path: "/",
+      component: Home,
     },
     {
-      path: '/login',
-      component: Login
+      path: "/login",
+      component: Login,
     },
     {
-      path:'/b/:bid', component:Board
-  },
+      path: "/b/:bid",
+      component: Board,
+      children: [
+        {
+          path: "c/:cid",
+          component: Card,
+        }
+      ],
+    },
     {
-      path: '*',
-      component: NotFound
-      }
-  ]
-})
+      path: "*",
+      component: NotFound,
+    },
+  ],
+});
 
-export default router // 사용할 수 있도록 router export
-
+export default router; // 사용할 수 있도록 router export
