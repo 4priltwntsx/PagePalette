249dbf592b03fb2bd6431d2e0c196cf477ee9c14
김지희 <cjg05034@naver.com>
2024-02-11 13:54:58 +0900

    feat: add feature that connect bookInfo to AddLsit

--- page-palette-front/src/components/list/AddList.vue
+++ page-palette-front/src/components/list/AddList.vue
@@ -11,66 +11,62 @@
     </a>
   </div> -->
 
-    <Modal>
-      <div slot="header">
-        <h2>
-          new List of your Book
-          <a href="" class="modal-default-button" 
-            @click.prevent="SET_IS_ADD_LIST(false)">&times;</a>
-        </h2>
+  <Modal >
+    <div slot="header">
+      <h2>
+        new List of your Book
+        <a href="" class="modal-default-button" @click.prevent="SET_IS_ADD_LIST(false)">&times;</a>
+      </h2>
+    </div>
+    <div slot="body"  >
+      <form id="add-board-form" @submit.prevent="searchBook">
+        <input class="form-control" type="text" v-model="input" ref="input">
+      </form>
+      <div  v-if="isBooks" style="overflow-y: auto;"> 여기에 출력
+        <ul class="finded-books">
+          <BookItems v-for="book, i in this.books.bookList" :key="i" v-bind:book="book" />
+        </ul>
       </div>
-      <div slot="body">
-        <form id="add-board-form" 
-          @submit.prevent="searchBook">
-          <input class="form-control" type="text" v-model="input" ref="input">
-          
-        </form>
-          <!-- 수정된 부분 시작 -->
-
-      <!-- 수정된 부분 끝 -->
-      </div>
-      <div slot="footer">
-        <button class="btn" :class="{'btn-success': valid}" type="submit" 
-          form="add-board-form" :disabled="!valid">
-          Create List</button>
-      </div>
-    </Modal>
+    </div>
+    <div slot="footer">
+      <button class="btn" :class="{ 'btn-success': valid }" type="submit" form="add-board-form" :disabled="!valid">
+        Create List</button>
+    </div>
+  </Modal>
 </template>
 
 <script>
-import {mapState, mapMutations, mapActions} from 'vuex'
+import { mapState, mapMutations, mapActions } from 'vuex'
 import Modal from '../common/Modal.vue'
+import BookItems from './BookItems.vue'
 
 export default {
-  components:{
-    Modal
+  components: {
+    Modal, BookItems
   },
   data() {
     return {
       isAddList: false,
       inputTitle: '',
-      input:'',
+      input: '',
       valid: false,
-      bs:[],
+      books: [],
+      isBooks: true,
     }
   },
-  watch:{
-    input(v){
-      this.valid=v.trim().length>0
+  watch: {
+    input(v) {
+      this.valid = v.trim().length > 0
     },
-    books(){
-      console.log(this.$store.state.books)
-    }
 
   },
   computed: {
     ...mapState({
       board: 'board',
       list: 'list',
-      books: [],
     })
   },
-  mounted(){
+  mounted() {
     this.$refs.input.focus()
   },
   methods: {
@@ -82,16 +78,16 @@ export default {
       'ADD_LIST',
       'FETCH_BOOK',
     ]),
-    async searchBook(){
+    async searchBook() {
       console.log("!!= 들어간다!!!!" + this.input)
-      this.bs = await this.FETCH_BOOK({title: this.input})
-      console.log( this.bs.bookList) 
-      this.SET_BOOKS(this.bs.bookList)
-    }, 
+      this.books = await this.FETCH_BOOK({ title: this.input })
+      console.log(this.books.bookList)
+      this.SET_BOOKS(this.books.bookList)
+    },
     addList() {
       console.log(this.input)
       this.isAddList = true
-      this.$nextTick(_=> this.$refs.inputTitle.focus())
+      this.$nextTick(_ => this.$refs.inputTitle.focus())
 
       this.SET_IS_ADD_LIST(false)
     },
@@ -102,10 +98,10 @@ export default {
       const title = this.inputTitle
       const curList = this.$store.state.list
       const lastList = curList[curList.length - 1]
-    //   const pos = lastList ? lastList.pos * 2 : 65535
-    const bookIsbn = "9791168473690"
+      //   const pos = lastList ? lastList.pos * 2 : 65535
+      const bookIsbn = "9791168473690"
       const boardId = this.$store.state.board.boardId
-      this.ADD_LIST({bookTitle: title, bookIsbn, boardId}).then(_ => this.restore())
+      this.ADD_LIST({ bookTitle: title, bookIsbn, boardId }).then(_ => this.restore())
     },
     restore() {
       this.isAddList = false
@@ -117,19 +113,26 @@ export default {
 
 <style>
 .add-list {
-  background-color: rgba(0,0,0, .1);
+  background-color: rgba(0, 0, 0, .1);
   padding: 12px;
   color: #ddd;
   transition: all .3s;
 }
+
 .add-list a {
-    text-decoration: none;
-    color: white;
-    font-weight: 600;
+  text-decoration: none;
+  color: white;
+  font-weight: 600;
 }
+
 .add-list:hover,
 .add-list:focus {
-  background-color: rgba(0,0,0, .3);
+  background-color: rgba(0, 0, 0, .3);
   cursor: pointer;
+}
+
+.finded-books{
+    width: 60%;
+    max-height: 400;
 }
 </style>
---
+++ page-palette-front/src/components/list/BookItems.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="book-item">
+        <a href="" @click="AddList">{{ book.bookTitle }}</a>
+    </div>
+</template>
+
+<script>
+import { mapState, mapMutations, mapActions } from 'vuex'
+
+export default {
+    props: ['book'],
+
+    data() {
+    return {
+      isAddList: false,
+      inputTitle: '',
+      input: '',
+      valid: false,
+      books: [],
+      isBooks: true,
+    }
+  },
+}
+</script>
+
+<style>
+.book-item{
+    width:40%;
+}
+.book-item a {
+  text-decoration: none;
+  color: rgba(0, 0, 0, 0.705);
+  font-weight: 600;
+}
+</style>
+
